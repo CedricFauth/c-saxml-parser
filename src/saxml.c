179cb8870ad83bca9f206dcd145e2c0fff89b3ad
Cedric Fauth <cedricfauth@yahoo.de>
2020-12-18 19:50:40 +0100

    closing fp

--- src/saxml.c
+++ src/saxml.c
@@ -11,30 +11,35 @@ char *read_file(char *path) {
     }
     if (fseek(fp, 0L, SEEK_END) == -1) {
 		perror("fseek failed");
+        fclose(fp);
 		return NULL;
 	}
     long length = ftell(fp);
 	if (length == -1L) {
 		perror("ftell failed");
+        fclose(fp);
 		return NULL;
 	}
     if (fseek(fp, 0L, SEEK_SET) == -1) {
 		perror("fseek failed");
+        fclose(fp);
 		return NULL;
 	}
     char *data = malloc(sizeof(char) * (length+1));
     if (!data) {
         fprintf(stderr, "allocation failed: not enough memory %lu", length);
+        fclose(fp);
         return NULL;
     }
     size_t read = fread(data, sizeof(char), length, fp);
-    fclose(fp);
     data[length] = '\0';
     if (read != length) {
+        fclose(fp);
         free(data);
         fprintf(stderr,"read failed: %lu bytes but %lu expected\n", length, read);
         return NULL;
     }
+    fclose(fp);
 	printf("read: %zu bytes\n", read);
     return data;
 }
